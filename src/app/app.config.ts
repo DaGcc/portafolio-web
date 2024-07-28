f7e500c40abf52e7492a053e5c8beb28c61aa8fb
DaGcc <gutierrezcallazacadaniel2020@gmail.com>
2024-07-28 00:30:42 -0500

    280724

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withHashLocation, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -8,7 +8,7 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   
   providers: [
-    provideRouter(routes,withViewTransitions()),
+    provideRouter(routes,withViewTransitions(), withHashLocation()),
     // provideClientHydration(),
     // provideAnimations(),
     provideHttpClient(
